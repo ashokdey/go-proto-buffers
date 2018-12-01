7b90d5186dcf532aab604ba8ce6b588256844390
Ashok Dey <ashokdey100@gmail.com>
2018-12-02 01:42:45 +0530

    --update: plying with proto structure

--- main.go
+++ main.go
@@ -2,8 +2,26 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/go-proto-buffers/src/proto"
 )
 
 func main() {
-	fmt.Println("Hello from Go")
+	doSimple()
+}
+
+func doSimple() {
+	sm := simplepb.Simple{
+		Id:         12345,
+		IsSimple:   true,
+		Name:       "My simple message",
+		SampleList: []int32{1, 4, 7, 8},
+	}
+	fmt.Println(sm)
+
+	// we can manipulate the attributes
+	sm.Name = "Renamed my message"
+	fmt.Println(sm)
+
+	fmt.Println("The message id = ", sm.GetId())
 }
